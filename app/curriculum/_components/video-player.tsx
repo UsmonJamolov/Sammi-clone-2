61625c218492c6151cd0dad0f4e4e48b315493d3
usman <usman@example.com>
2025-11-14 08:16:55 +0300

    Review modal & ReactStars component

--- app/curriculum/_components/video-player.tsx
+++ app/curriculum/_components/video-player.tsx
@@ -1,41 +1,42 @@
-'use client'
+'use client';
 
 import { ComponentRef, useEffect, useRef } from 'react';
 import Vimeo from '@vimeo/player';
 
 interface VideoPlayerProps {
-    videoId: string
+	videoId: string;
 }
 
-const VideoPlayer = ({videoId}: VideoPlayerProps) => {
-    const vimePlayerRef = useRef<ComponentRef<'div'>>(null)
+const VideoPlayer = ({ videoId }: VideoPlayerProps) => {
+	const vimePlayerRef = useRef<ComponentRef<'div'>>(null);
 
-    useEffect(() => {
-        if (vimePlayerRef.current) {
-            const player = new Vimeo(vimePlayerRef.current, {
-                id: parserInt(videoId),
-                responsive: true
-            })
+	useEffect(() => {
+		if (vimePlayerRef.current) {
+			const player = new Vimeo(vimePlayerRef.current, {
+				id: parseInt(videoId),
+				responsive: true,
+			});
 
-            player
-                .ready()
-                .then(() => {
-                    // Player is ready
-                })
-                .catch(error => {
-                    console.error();
-                    
-                })
+			player
+				.ready()
+				.then(() => {
+					// Player is ready
+				})
+				.catch(error => {
+					console.error('Error initializing Vimeo player:', error);
+				});
 
-            player.on('ended', () => onEnded())
-        }
-    }, onEnded = () => {})
+			player.on('ended', () => onEnded());
+		}
+	}, [videoId]);
 
-    return (
-        <div>
-            <div ref={vimePlayerRef}></div>
-        </div>
-    )
-}
+	const onEnded = () => {};
 
-export default VideoPlayer
+	return (
+		<div>
+			<div ref={vimePlayerRef}></div>
+		</div>
+	);
+};
+
+export default VideoPlayer;
